3ff50dcbe2f46a83ab0c504f6a7c1dc372503963
skywalker-97 <rajgaurav7250@gmail.com>
2026-08-21 18:36:40 +0530

    Fix CORS strict origin issue

--- server/server.js
+++ server/server.js
@@ -15,21 +15,8 @@ connectDB();
 app.use(helmet({ crossOriginResourcePolicy: false }));
 
 // CORS
-const allowedOrigins = [
-  'http://localhost:5173',
-  'http://localhost:3000',
-  'https://razorpay-commerce-agent.onrender.com', // Expected Render frontend URL
-  process.env.CLIENT_URL
-].filter(Boolean);
-
 app.use(cors({
-  origin: function (origin, callback) {
-    if (!origin || allowedOrigins.includes(origin)) {
-      callback(null, true);
-    } else {
-      callback(new Error('Not allowed by CORS'));
-    }
-  },
+  origin: true,
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization', 'X-Session-Id'],
